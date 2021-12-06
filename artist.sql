2285a2e97722cea64e0d199f27ab8a1643137711
Connor Barry <cbarry578@gmail.com>
2021-12-06 14:03:14 -0800

    Finished artist

---
+++ artist.sql
@@ -0,0 +1,16 @@
+--problem 1
+-- INSERT INTO artist(name)
+-- VALUES('Weezer'),('blink-182'),('Kanye-West');
+
+--problem 2
+--SELECT * FROM artist WHERE artist_id < 11 ORDER BY name DESC ;
+
+--problem 3
+--SELECT * FROM artist WHERE artist_id < 6 ORDER BY name ASC ;
+
+--problem 4
+--SELECT * FROM artist WHERE name LIKE  'Black%';
+
+--problem 5
+--SELECT * FROM artist WHERE name LIKE  '%Black%';
+
